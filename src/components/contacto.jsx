e8bf1b9648ac3fbaab7a276014207cf67dee9e86
netoba1 <ernestonetoba@gmail.com>
2023-11-20 17:12:40 -0600

    cambio

---
+++ src/components/contacto.jsx
@@ -0,0 +1,14 @@
+// ContactoCard.js
+import React from "react";
+
+const Contacto = () => {
+  return (
+    <div>
+      {/* Contenido del componente de Contacto */}
+      <h2>Contacto</h2>
+      {/* ...otros códigos */}
+    </div>
+  );
+};
+
+export default Contacto;
